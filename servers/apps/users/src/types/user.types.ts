3c192f7dedb6617bd8f9e5cdb14db1848cd6062e
ishi12121 <isht1402@gmail.com>
2024-05-11 00:15:00 +0530

    initial commit

---
+++ servers/apps/users/src/types/user.types.ts
@@ -0,0 +1,30 @@
+import { ObjectType, Field } from '@nestjs/graphql';
+import { User } from '../entities/user.entity';
+
+@ObjectType()
+export class ErrorType {
+  @Field()
+  message: string;
+
+  @Field({ nullable: true })
+  code?: string;
+}
+
+@ObjectType()
+export class RegisterRepose {
+  @Field(() => User, { nullable: true })
+  user?: User | any;
+
+  @Field(() => ErrorType, { nullable: true })
+  error?: ErrorType;
+}
+
+@ObjectType()
+export class LoginResponse {
+    @Field(() => User, { nullable: true })
+    user?: User | any;
+    
+    @Field(() => ErrorType, { nullable: true })
+    error?: ErrorType;
+}
+
